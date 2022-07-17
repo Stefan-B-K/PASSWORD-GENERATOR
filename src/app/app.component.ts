eccf6d495112a7e5008860656ed3bdc542033e09
iStef <stefan_b_k@icloud.com>
2022-07-17 19:31:05 +0300

    init/final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+     selector: 'app-root',
+     templateUrl: './app.component.html',
+     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Password-Generator';
+     password = ''
+     passLength = 0
+     includeLetters = true
+     includeNumbers = false
+     includeSymbols = false
+
+     get formIsValid (): boolean {
+          return (this.includeLetters || this.includeNumbers || this.includeSymbols)
+               && this.passLength > 5 && this.passLength < 31
+     }
+
+     setPassLength (input: EventTarget | null) {
+          const length = parseInt((input as HTMLInputElement).value)
+          if (!isNaN(length)) this.passLength = length
+     }
+
+     onButtonClick (): void {
+          this.password = ''
+          const numbers = '1234567890'
+          const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+          const symbols = '!@#$%^&*()±§[]<>'
+          const validChars = (this.includeLetters ? letters : '') +
+               (this.includeNumbers ? numbers : '') + (this.includeSymbols ? symbols : '')
+          for (let i = 0; i < this.passLength; i++) {
+               this.password += validChars[Math.floor(Math.random() * validChars.length)]
+          }
+     }
 }
